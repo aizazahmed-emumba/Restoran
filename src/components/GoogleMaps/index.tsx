c993e4aaa375b82a1984c2c8b61efd401994b635
aizazahmed-emumba <aizaz.ahmed@emumba.com>
2024-06-20 17:40:55 +0500

    fix: map

--- src/components/GoogleMaps/index.tsx
+++ src/components/GoogleMaps/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { useState } from "react";
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow,
-} from "@vis.gl/react-google-maps";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 const GoogleMaps: React.FC = () => {
   const position = { lat: 37.7749295, lng: -122.4194155 };
